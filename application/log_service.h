1b4397490d3f1cfcf8f73374999f0f669c8af837
river <hello.river@outlook.com>
2026-06-15 19:32:44 +0800

    feat: add log service implementation with log_printf function

---
+++ application/log_service.h
@@ -0,0 +1,16 @@
+#ifndef _LOG_SERVICE_H__
+#define _LOG_SERVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+
+void log_printf(char *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LOG_SERVICE_H__ */
